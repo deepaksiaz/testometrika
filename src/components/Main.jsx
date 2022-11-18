804e9a29f5b2a7719f9b6ab1eba64a94b2cf20e4
deepaksiaz <101709977+deepaksiaz@users.noreply.github.com>
2022-11-18 14:04:16 +0530

    commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+import { Button } from "antd";
+
+const Main = () => {
+  return (
+    <div className="main-page">
+      <Button className="button-1" type="none" style={{}}>
+        START TEST
+      </Button>
+    </div>
+  );
+};
+export default Main;
